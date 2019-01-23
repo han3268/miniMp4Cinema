a8a87c37482021c52c25bad1dcbcac3d0667a1e4
han3268 <han3268@outlook.com>
2019-01-23 19:43:33 +0800

    纯静态第一次提交

---
+++ js/header.js
@@ -0,0 +1,14 @@
+$(function(){
+  $.ajax({
+    url:"../header.html",
+    type:"get",
+    success:function(res){
+      //动态添加<link>到<head>元素中，自动引入header.css
+      $("<link rel='stylesheet' href='css/header.css'>").appendTo("head");
+      //用接收到的片段代替页面上空的<header>
+      //<header>
+      $(res).replaceAll("#header");
+      console.log(res)
+    }
+  })
+})
---
+++ js/index.js
@@ -0,0 +1,3 @@
+$(".mynav-tab div img").hover(function(){/*轮播 */
+  $(".mynav-tab>div>a>img")[0].src = this.src
+})
