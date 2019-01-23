f5ec18e2fa104ace700f3be41c06654fd7a2949d
han3268 <han3268@outlook.com>
2019-01-23 22:01:05 +0800

    报错

--- js/header.js
+++ js/header.js
@@ -1,14 +1,32 @@
-$(function(){
-  $.ajax({
-    url:"../header.html",
-    type:"get",
-    success:function(res){
-      //动态添加<link>到<head>元素中，自动引入header.css
-      $("<link rel='stylesheet' href='css/header.css'>").appendTo("head");
-      //用接收到的片段代替页面上空的<header>
-      //<header>
-      $(res).replaceAll("#header");
-      console.log(res)
-    }
-  })
+
+$.ajax({
+  url:"../header.html",
+  type:"get",
+  success:function(res){
+    //动态添加<link>到<head>元素中，自动引入header.css
+    // $("<link rel='stylesheet' href='css/header.css'>").appendTo("head");
+    //用接收到的片段代替页面上空的<header>
+    $(res).replaceAll("#header");   
+  }
 })
+
+function searchBut(e){
+  e.preventDefault();
+  console.log($('.mynav ul>li'))
+}
+console.log($('.search-box')[0])
+
+
+
+$('.search-box').focus(function(){
+  console.log($('.mynav ul>li'))
+})
+
+
+$(".search-but").on("click",function(e){
+  e.preventDefault()
+  console.log($('.mynav ul>li'))
+})
+$(".search-but").click(function(event){
+  event.preventDefault();
+});
--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
 $(".mynav-tab div img").hover(function(){/*轮播 */
   $(".mynav-tab>div>a>img")[0].src = this.src
 })
+
