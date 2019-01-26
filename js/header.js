bf40bb41e2c764812e27807ed744639546ca3bf9
han3268 <han3268@outlook.com>
2019-01-26 14:17:41 +0800

    header 交互

--- js/header.js
+++ js/header.js
@@ -1,32 +1,41 @@
+$(function(){
+  $.ajax({
+    url:"./header.html",
+    type:"get",
+    success:function(res){
+      //动态添加<link>到<head>元素中，自动引入header.css
+      // $("<link rel='stylesheet' href='css/header.css'>").appendTo("head");
+      //用接收到的片段代替页面上空的<header>
+      $(res).replaceAll("#header");   
 
-$.ajax({
-  url:"../header.html",
-  type:"get",
-  success:function(res){
-    //动态添加<link>到<head>元素中，自动引入header.css
-    // $("<link rel='stylesheet' href='css/header.css'>").appendTo("head");
-    //用接收到的片段代替页面上空的<header>
-    $(res).replaceAll("#header");   
-  }
+      var atURL = window.location.pathname//获取地址栏的URl
+      var atURL = substr(atURL)//正则过滤 获取html 名字
+
+      var navs = $(".navbar ul>li a")
+      for(var item of  navs ){
+        var presentURL =substr($(item).attr("href"))//获取ul>li>a的URL
+        if( atURL == presentURL ){
+          $(item).parent().addClass("active")//父元素 li  class="active" 
+        }
+      }
+
+
+      $(".search-box").on("click",function(e){
+        e.preventDefault()
+        console.log($('.mynav ul>li'))
+      })
+      $(".search-but").click(function(event){
+        event.preventDefault();
+        console.log("111")
+      });
+    }
+  })
+
+ function substr(url){
+  var substr = url.match(/\/(\S*).htm/);
+  return substr[1]
+ }
+
 })
 
-function searchBut(e){
-  e.preventDefault();
-  console.log($('.mynav ul>li'))
-}
-console.log($('.search-box')[0])
 
-
-
-$('.search-box').focus(function(){
-  console.log($('.mynav ul>li'))
-})
-
-
-$(".search-but").on("click",function(e){
-  e.preventDefault()
-  console.log($('.mynav ul>li'))
-})
-$(".search-but").click(function(event){
-  event.preventDefault();
-});
--- js/index.js
+++ js/index.js
@@ -1,4 +1,27 @@
 $(".mynav-tab div img").hover(function(){/*轮播 */
-  $(".mynav-tab>div>a>img")[0].src = this.src
+  $(".mynav-tab>div>a>img").prop("src",this.src)
 })
 
+
+
+$(".search-but").click(function(e){
+  e.preventDefault()
+  $(this).toggleClass("d-none")
+})
+
+$(".alert-success").click(function(e){
+  e.preventDefault()
+  $(this).toggleClass("d-none")
+})
+
+
+//console.log()
+//$('.search-but').html("ss")
+// $(".search-but").on("click",function(e){
+//   e.preventDefault()
+//   console.log($('.mynav ul>li'))
+// })
+
+// $(".search-but").click(function(event){
+//   event.preventDefault();
+// });
